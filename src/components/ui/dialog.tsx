0c58e9a0cc873c98757b31db3bf1cd081e8b3f3c
Rizlan - Mitra <ahamedrislan2002@gmail.com>
2026-04-30 18:19:13 +0530

    modern scrollbar configured

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -32,12 +32,12 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 w-full max-w-md translate-x-[-50%] translate-y-[-50%] bg-white rounded-2xl shadow-2xl duration-300 ease-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] overflow-hidden max-h-[90vh] flex flex-col",
+        "fixed left-[50%] top-[50%] z-50 w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] bg-white rounded-2xl shadow-2xl duration-300 ease-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] overflow-hidden",
         className
       )}
       {...props}
     >
-      <div className="p-6 overflow-y-auto flex-1 scrollbar-thin scrollbar-thumb-indigo-400 scrollbar-track-gray-100">{children}</div>
+      <div className="p-8">{children}</div>
     </DialogPrimitive.Content>
   </DialogPortal>
 ));
@@ -58,7 +58,7 @@ DialogHeader.displayName = "DialogHeader";
 
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div 
-    className={cn("flex flex-col-reverse sm:flex-row sm:justify-end gap-3 mt-8 pt-6 border-t border-gray-100", className)} 
+    className={cn("flex flex-col-reverse sm:flex-row sm:justify-end gap-3 mt-8 pt-6 border-t border-gray-100 ", className)} 
     {...props} 
   />
 );
